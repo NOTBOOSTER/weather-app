3d88b4b30eaf2bc3ec81e1a34921b8ed428ac56b
Bhaskar Melkani <iamnotbooster@gmai.com>
2024-09-10 21:23:08 +0530

    some big improvement

--- src/App.jsx
+++ src/App.jsx
@@ -5,72 +5,42 @@ import axios from "axios";
 
 function App() {
   const API_KEY = "9105cfa2fc93f981deda3f3d1db8d163";
-  const [Location, setLocation] = useState("haldwani");
+
+  
+  const setlastLocation = (Location) => {
+    localStorage.setItem("lastLocation", Location);
+  }
+
+  const getLastLocation = () => {
+    return localStorage.getItem("lastLocation") || "Delhi";
+  }
+
+  const [Location, setLocation] = useState(getLastLocation);
   const [forecastData, setForecast] = useState([]);
 
-// dummy api response
 
-  const [weatherData, setWeather] = useState({
-    coord: {
-      lon: 0,
-      lat: 0,
-    },
-    weather: [
-      {
-        id: 800,
-        main: "Clear",
-        description: "Clear sky",
-        icon: "01d",
-      },
-    ],
-    base: "stations",
-    main: {
-      temp: 20,
-      feels_like: 20,
-      temp_min: 20,
-      temp_max: 20,
-      pressure: 1013,
-      humidity: 50,
-      sea_level: 1013,
-      grnd_level: 1013,
-    },
-    visibility: 10000,
-    wind: {
-      speed: 0,
-      deg: 0,
-    },
-    clouds: {
-      all: 0,
-    },
-    dt: 1699132800,
-    sys: {
-      type: 1,
-      id: 0,
-      country: "XX",
-      sunrise: 1699118000,
-      sunset: 1699148800,
-    },
-    timezone: 0,
-    id: 0,
-    name: "Dummy City",
-    cod: 200,
-  });
 
-// get weather api data
+  const [weatherData, setWeather] = useState({});
+
+  useEffect(() => {
+    getWeather();
+    fetchData();
+  }, []);
+
+  // get weather api data
 
   const getWeather = async () => {
+    setlastLocation(Location);
     await axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${Location}&units=metric&appid=${API_KEY}`
       )
       .then((response) => {
-        console.log(response);
+        console.log(response.data);
         setWeather(response.data);
       })
       .catch((error) => console.log(error));
   };
-
-  
 
   const fetchData = async () => {
     try {
@@ -81,28 +51,36 @@ function App() {
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
-
   };
 
   const Data = (apidata) => {
     return apidata.filter((item) => item.dt_txt.includes("12:00:00"));
-  }
+  };
 
   return (
     <div className=" flex items-center justify-center bg-bgimg sm:h-screen sm:w-screen bg-no-repeat bg-cover flex-col bg-center bg-fixed">
-      <div className="max-w-1/2 h-fit py-12 rounded-xl backdrop-blur bg-[#ffffff76]">
-        <Search
-          getWeather={getWeather}
-          setLocation={setLocation}
-          fetchData={fetchData}
-        />
-        <Weather
-          weatherData={weatherData}
-          forecastData={forecastData}
-        />
-      </div>
-      
-      <footer className="font-bold mt-14">Made By <a className="text-red-500" href="https://github.com/NOTBOOSTER/weather-app">Bhaskar Melkani</a></footer>
+      {weatherData && weatherData.main ? (
+        <div className="max-w-1/2 h-fit py-12 rounded-xl backdrop-blur bg-[#ffffff76]">
+          <Search
+            getWeather={getWeather}
+            setLocation={setLocation}
+            fetchData={fetchData}
+          />
+          <Weather weatherData={weatherData} forecastData={forecastData} />
+        </div>
+      ) : (
+        <span>loading</span>
+      )}
+
+      <footer className="font-bold mt-14">
+        Made By{" "}
+        <a
+          className="text-red-500"
+          href="https://github.com/NOTBOOSTER/weather-app"
+        >
+          Bhaskar Melkani
+        </a>
+      </footer>
     </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,34 @@
 import { IoMdSearch } from "react-icons/io";
 
-const Search = ({getWeather, fetchData, setLocation}) => {
+const Search = ({ getWeather, fetchData, setLocation }) => {
+  
+  const handleKey = (key) => {
+    if (key.key === "Enter") {
+      fetchData();
+      getWeather(); 
+    }
+  };
+
   return (
     <div className=" flex justify-center">
-      <input onChange={(e)=>{setLocation(e.target.value)}} type="search" placeholder="Enter Location" className="focus:outline-none shadow-inner shadow-gray-600 focus:shadow-gray-900 h-12 md:w-96 w-56 rounded-lg px-5 placeholder-gray-500"></input>
-      <button onClick={()=>{fetchData(); getWeather()}} className="ml-2 bg-white px-4 rounded-lg shadow-inner shadow-gray-600 hover:shadow-gray-800"><IoMdSearch size="20" color="#343333"/></button>
+      <input
+        onKeyDown={handleKey}
+        onChange={(e) => {
+          setLocation(e.target.value);
+        }}
+        type="search"
+        placeholder="Enter Location"
+        className="focus:outline-none shadow-inner shadow-gray-600 focus:shadow-gray-900 h-12 md:w-96 w-56 rounded-lg px-5 placeholder-gray-500"
+      ></input>
+      <button
+        onClick={() => {
+          fetchData();
+          getWeather();
+        }}
+        className="ml-2 bg-white px-4 rounded-lg shadow-inner shadow-gray-600 hover:shadow-gray-800"
+      >
+        <IoMdSearch size="20" color="#343333" />
+      </button>
     </div>
   );
 };
